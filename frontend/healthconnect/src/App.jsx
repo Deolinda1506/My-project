136cf5b0d0a8437711eb10311f7bf309d7fdc220
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2024-11-15 13:36:33 +0000

    fine

--- frontend/healthconnect/src/App.jsx
+++ frontend/healthconnect/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PatientDash from "./components/dashboard/PatientDash";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import { PollingProvider } from "./components/appointments/PollingContext";
+import VideoCall from "./components/appointments/VideoCall";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster />
+      <PollingProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/call" element={<VideoCall />} />
+            <Route path="/*" element={<div>404</div>} />
+          </Routes>
+        </BrowserRouter>
+      </PollingProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/healthconnect/src/main.jsx
+++ frontend/healthconnect/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './app/store'; // Import both store and persistor
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+);
